1118116765940d3596c532689bfaf5d3dabc6b77
Meryem <115747292+MeryemBugur@users.noreply.github.com>
2024-01-12 16:19:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+      document.addEventListener('DOMContentLoaded', function() {
+
+        console.log("The document was loaded.");
+
+        function showAlert() {
+          alert('Welcome to ShoWell!');
+        }
+
+        showAlert();
+      });
